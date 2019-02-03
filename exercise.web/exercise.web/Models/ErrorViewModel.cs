9478aa646886560b9a6b1888703701b6ea0bba19
Safiullah <dev.rupaisafi@gmail.com>
2019-02-03 11:38:15 +0600

    Business1

---
+++ exercise.web/exercise.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exercise.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
